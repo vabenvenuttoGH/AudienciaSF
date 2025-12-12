27bd118fe6f5c50d624d78a73c31b79afb6909d2
Valentino <vbenvenutto28@gmail.com>
2025-12-11 23:12:45 -0300

    Eliminar archivos login.jsx y login.css antiguos

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import LoginPage from './login';
+import LoginPage from './components/Login/Login';
 
 function App() {
   return (
--- frontend-react/src/components/Login/Login.jsx
+++ frontend-react/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./login.css";
+import "./Login.css";
 
 export default function LoginPage() {
   return (
